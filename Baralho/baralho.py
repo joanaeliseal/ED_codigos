2a4af76444b14d192768c5139988ba8702bdd7fe
Joana Elise <112410414+joanaeliseal@users.noreply.github.com>
2024-03-25 13:53:14 -0300

    correções usando código do prof

--- Baralho/baralho.py
+++ Baralho/baralho.py
@@ -1,52 +1,82 @@
-from carta import Carta
-import random
+from enum import Enum
+from random import shuffle
+
+class Naipe(Enum):
+    OUROS = 1
+    PAUS = 2
+    ESPADAS = 3
+    COPAS = 4
+
+
+class Carta:
+    AS = 1
+    VALETE = 11
+    DAMA = 12
+    REI = 13
+    
+    def __init__(self, valor: int, naipe: Naipe):
+        self.__valor = valor
+        self.__naipe = naipe
+
+    def _get_valor_como_string(self):
+        match self.__valor:
+            case Carta.AS:
+                return "Ás"
+            case Carta.REI:
+                return "Rei"
+            case Carta.DAMA:
+                return "Dama"
+            case Carta.VALETE:
+                return "Valete"
+            case _:
+                return str(self.__valor)
+            
+    def _get_naipe_como_string(self):
+        return self.__naipe.name.title()
+        
+    def __repr__(self):
+        return f'Carta({self._get_valor_como_string()} de {self._get_naipe_como_string()})'
+
 
 class Baralho:
-    """
-    Inicialização cria o atributo cartas e gera 52 cartas p/ baralho
-        laço aninhado:
-        - laço externo enumera os naipes de 0 a 3
-        - laço interno enumera as posições de 1 até 13
-    Total de execuções: 52, totalizando as 52 cartas
-    Cada iteração cria uma nova instância de Carta com o naipe e posição atuais e a inclui na lista cartas
-    """
-    def __init__(self):
-        self.cartas = []
-        for naipe in range(4):
-            for posicao in range(1, 14):
-                self.cartas.append(Carta(naipe, posicao)) # método append é para listas, não tuplas
-
-    def embaralha(self):
-        """ Método que percorre a lista e troca cada carta por outra
-        escolhida aleatoriamente. É possível que a carta seja trocada por ela mesma. 
-        Para trocar as cartas, usa-se uma atribuição de tupla
-        """
-        numero_cartas = len(self.cartas)
-        for i in range(numero_cartas):
-            j = random.raandrange(i, numero_cartas)
-            self.cartas[i], self.cartas[j] = self.cartas[j], self.cartas[i]
-        # método shuffle serve para embaralhar cartas
-            
-    def remover_carta(self, carta):
-        if carta in self.cartas:
-            self.cartas.pop(carta)
-            return True
-        else:
-            return False
-
-    def add_carta(self, posicao, naipe):
-        pass
-
-    def esta_vazio(self):
-        return (len(self.cartas) == 0)
-
-    def imprimirBaralho(self):
-        for carta in self.cartas:
-            print(carta)
-            #pq nao usar o __str__?
     
-    # def __str__(self):
-    #     s = ""
-    #     for i in range(len(self.cartas)):
-    #         s = s + " "*i + str(self.cartas[i]) + "\n"
-    #     return s
+    def __init__(self):
+        self.__cartas = []
+        for naipe in Naipe.__members__:
+            for valor in range(1, 14):
+                self.__cartas.append(Carta(valor, Naipe[naipe]))
+        self.embaralhar()
+
+    @property
+    def cartas(self):
+        return self.__cartas
+    
+    def embaralhar(self):
+        shuffle(self.__cartas)
+    
+    def __len__(self):
+        return len(self.__cartas)
+    
+    def distribuir(self, jogadores: list['Jogador']):
+        total_jogadores = len(jogadores)
+        cartas_por_jogador = len(self) // total_jogadores
+        for indice, jogador in enumerate(jogadores):
+            comeco = indice * cartas_por_jogador
+            fim = comeco + cartas_por_jogador
+            jogador.receber_cartas(self.cartas[comeco: fim])
+
+class Jogador:
+
+    def __init__(self, nome):
+        self.__nome = nome
+        self.__cartas = []
+
+    @property
+    def cartas(self):
+        return self.__cartas
+
+    def receber_cartas(self, cartas: list[Carta]):
+        self.__cartas.extend(cartas)
+
+    def __repr__(self):
+        return f'Jogador({self.__nome})'
